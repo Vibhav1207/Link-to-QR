52d2dbb926b4ef8d67cd2436ffd4bbe0d9e87a92
Vibhav Patel <77201529+Vibhav1207@users.noreply.github.com>
2025-08-09 13:03:49 +0530

    a

--- script.js
+++ script.js
@@ -75,7 +75,7 @@ $(document).ready(function() {
             downloadLink.attr('download', 'qr_code_with_logo.png');
             downloadLink.show();
         };
-        logo.src = 'Uday.png';
+        logo.src = 'SEDC.jpg';
     }
 
     function hexToRgb(hex) {
