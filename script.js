24c3d4a16f5ebdb5d2aff7682545010a43c69fbe
Vibhav Patel <77201529+Vibhav1207@users.noreply.github.com>
2025-11-01 15:16:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -75,7 +75,7 @@ $(document).ready(function() {
             downloadLink.attr('download', 'qr_code_with_logo.png');
             downloadLink.show();
         };
-        logo.src = 'SIH.png';
+        logo.src = 'Uday.png';
     }
 
     function hexToRgb(hex) {
